c2f19a0b1feff6e8d3b5f59daabdc8118c8c1cf2
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-07-18 14:51:59 +0300

    Upload Project

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <h1>Hello World</h1>
-    </div>
-  );
+	return (
+		<div className="main-div">
+			<h1>Hello World</h1>
+		</div>
+	);
 }
